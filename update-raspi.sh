c5ac203174d1208c5d5f4dba91ef71dbd74a2400
babism-dev <75264925+babism-dev@users.noreply.github.com>
2020-11-30 17:28:51 +0100

    done
    done

---
+++ update-raspi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+####################################
+#
+# Update and Reboot by Babis.M Raspi
+#
+####################################
+sudo apt update
+sudo apt full-upgrade
+sudo reboot
