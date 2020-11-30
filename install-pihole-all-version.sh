3936f92691c4c0bb4c0ad6bae1e4575ed4b60e26
babism-dev <75264925+babism-dev@users.noreply.github.com>
2020-11-30 17:34:35 +0100

    Create install-pihole-all-version.sh
    done

---
+++ install-pihole-all-version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#######################################
+#
+# Get pihole latest Version from Git
+#
+#######################################
+sudo curl -sSL https://install.pi-hole.net | bash
+exit
